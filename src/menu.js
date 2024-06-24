8a421cc466bff31f82fb707f71971e969ccc2289
Jimmy <121305329+quuixlie@users.noreply.github.com>
2024-06-24 21:50:25 +0200

    Add project showcase functionality

--- src/menu.js
+++ src/menu.js
@@ -9,3 +9,49 @@ function closeMenu() {
     menu.style.display = "none";
     document.querySelector("body").style.overflow = "auto";
 }
+
+// Project showcase
+
+// Drag
+const gallery = document.querySelector("#gallery");
+let isDragging = false;
+let startX, startScrollLeft;
+
+
+const dragStart = (e) => {
+    isDragging = true;
+    gallery.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = gallery.scrollLeft;
+}
+
+const dragStop = () => {
+    isDragging = false;
+    gallery.classList.remove("dragging");
+}
+
+const dragging = (e) => {
+    if (!isDragging) return;
+    gallery.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+gallery.addEventListener("mousedown", dragStart);
+gallery.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+
+// Buttons
+const arrowLeft = document.querySelector("#arrow-left");
+const arrowRight = document.querySelector("#arrow-right");
+const cartWidth = document.querySelector(".cart").offsetWidth;
+
+const previousProject = () => {
+    gallery.scrollLeft -= cartWidth;
+}
+
+const nextProject = () => {
+    gallery.scrollLeft += cartWidth;
+}
+
+arrowLeft.addEventListener("click", previousProject);
+arrowRight.addEventListener("click", nextProject);
+// Project showcase end
